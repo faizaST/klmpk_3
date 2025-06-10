c1780cfbfa23c200c41a94c9fae6155e94b51842
mifta00 <miftata00@gmail.com>
2025-06-10 20:28:04 +0700

    menambahkan registrasi

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/auth/login_page.dart';
+import 'screens/auth/registrasi_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: LoginPage()
+      home: RegistrasiPage()
     );
   }
 }
