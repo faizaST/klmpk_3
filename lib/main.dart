a885ee5f49c4952523f19007845837e3c3d15524
AdityanM13 <220102002@mhs.udb.ac.id>
2025-06-10 23:02:04 +0700

    menambahkan profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:klmpk_3/screens/profile/profil_page.dart';
 import 'screens/splash/splashscreen_page.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: SplashscreenPage()
+      home: ProfilPage()
     );
   }
 }
