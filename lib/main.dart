9f0b7a46adc83a952daa46cddc2418aae02a4b9d
faizaST <faiza.tiaramadhan@gmail.com>
2025-06-11 13:21:06 +0700

    menambahkan fitur laporan

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:klmpk_3/screens/admin/laporan_page.dart';
 import 'package:klmpk_3/screens/admin/pemesanan_full_page.dart';
 import 'package:klmpk_3/screens/admin/pengguna_page.dart';
 import 'screens/admin/admin_home_page.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
   runApp(const MyApp());
 }
 
@@ -18,7 +22,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: PemesananFullPage(),
+      home: LaporanPage(),
     );
   }
 }
